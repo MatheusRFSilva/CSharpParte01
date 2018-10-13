549f39e0614bacd01ae008cddaefededb6dbe9e4
dragongx10 <matheusuniversal@yahoo.com.br>
2018-10-13 20:40:13 -0300

    4-Conversoes e Outros Tipos Numericos

---
+++ 4-ConversoesEOutrosTipoNumericos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTipoNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            double salario= 1200.50;
+
+            // o int é um tipo de variavel de 32 bits
+            int salarioEmInteiro;
+            salarioEmInteiro = (int) salario;
+
+            Console.WriteLine(salarioEmInteiro);
+            //O long é um tipo de variavel de 64 bits
+            long idade  = 13000000000000000;
+            Console.WriteLine(idade);
+
+            //O short é uma variavel de 16 bits;
+            short quantidadeProdutos = 19999;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.Read();
+        }
+    }
+}
